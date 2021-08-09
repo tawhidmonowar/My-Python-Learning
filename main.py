d8436f8b510c14f8d581d1db381617a4fa11223e
tawhidmonowar <tawhidmonowar@gmail.com>
2021-08-09 21:29:36 +0600

    8-9-2021

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-# This is a sample Python script.
+def translator(lang):
+    if lang == 'en':
+        return 'English'
+    elif lang == 'ba':
+        print('Bangla')
+    else: print('Hello World!')
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
+e = 'en'
+b = 'ba'
+print(translator(e),'language')
