059e0f03b03170594bc025b9cd945f9c0ab2c5ac
tawhidmonowar <tawhidmonowar@gmail.com>
2021-08-17 22:22:03 +0600

    8-17-2021

--- main.py
+++ main.py
@@ -1,10 +1,34 @@
-def translator(lang):
-    if lang == 'en':
-        return 'English'
-    elif lang == 'ba':
-        print('Bangla')
-    else: print('Hello World!')
+import pyttsx3
+import datetime
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+print(voices[0].id)
 
-e = 'en'
-b = 'ba'
-print(translator(e),'language')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = int(datetime.datetime.now().hour)
+
+    if 0 <= hour < 12:
+        speak("Good Morning! Let's get some coffee and start grinding")
+
+    elif 12 <= hour < 15:
+        speak("Good Afternoon, You are doing great")
+
+    elif 15 <= hour < 20:
+        speak("Good Evening. it's time for some tea")
+
+    else:
+        speak("Get recharged now. Good Night")
+
+    speak("I am Jarvis at your service")
+
+
+if __name__ == '__main__':
+    wishMe()
