d5d8718f7bf96b9983d30bdfd0fd3887a0e22722
tawhidmonowar <tawhidmonowar@gmail.com>
2021-08-22 18:16:30 +0600

    22-8-2021

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-import pyttsx3
-import datetime
-engine = pyttsx3.init('sapi5')
-voices = engine.getProperty('voices')
-print(voices[0].id)
-
-engine.setProperty('voice', voices[0].id)
-
-
-def speak(audio):
-    engine.say(audio)
-    engine.runAndWait()
-
-
-def wishMe():
-    hour = int(datetime.datetime.now().hour)
-
-    if 0 <= hour < 12:
-        speak("Good Morning! Let's get some coffee and start grinding")
-
-    elif 12 <= hour < 15:
-        speak("Good Afternoon, You are doing great")
-
-    elif 15 <= hour < 20:
-        speak("Good Evening. it's time for some tea")
-
-    else:
-        speak("Get recharged now. Good Night")
-
-    speak("I am Jarvis at your service")
-
-
-if __name__ == '__main__':
-    wishMe()
+data = "mail tawhidmonowar@gmail.com number: 01796938534"
+at = data.find("@")
+enDom = data.find(" ", at)
+doName = data[at:enDom]
+em = data.find(" ")
+emAd = data[em:at]
+print(emAd+doName)
