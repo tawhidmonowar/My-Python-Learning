011ce4dbcce233ac838e72fe5457a820afb41d33
tawhidmonowar <tawhidmonowar@gmail.com>
2021-08-26 22:32:04 +0600

    26-8-2021

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-data = "mail tawhidmonowar@gmail.com number: 01796938534"
-at = data.find("@")
-enDom = data.find(" ", at)
-doName = data[at:enDom]
-em = data.find(" ")
-emAd = data[em:at]
-print(emAd+doName)
+a = [20, 25, 30, 35]
+
+print(len(a))
+print(max(a))
+print(min(a))
+print(sum(a))
+
+
