15aa5abd13d3c08cf8fabebb752666b93bbd341c
tawhidmonowar <tawhidmonowar@gmail.com>
2021-09-09 00:12:49 +0600

    09-09-2021

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
-fileName = input('Enter the text file name: ')
-file = open(fileName)
-counts = dict()
+x = input('Enter Your Text: ')
+alphabet = {'a': 1, 'b': 2}
 
-for line in file:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
-print(counts)
 
-bigCount = 0
-bigWord = None
+def splitChar(word):
+    return [char for char in word]
 
-for word, count in counts.items():
-    if bigCount is None or count > bigCount:
-        bigWord = word
-        bigCount = count
-print('Big Word:', bigWord, '\nBig Count:', bigCount)
+
+y = splitChar(x.lower())
+
+for alphabet in y:
+    print(1)
